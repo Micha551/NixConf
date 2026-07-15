f208568c9afb9c7d3b5d26774959d6f240236a17
Michael Grinschewski <michas.grin@gmail.com>
2026-07-15 13:51:35 +0200

    feature: python

--- modules/python.nix
+++ modules/python.nix
@@ -4,10 +4,13 @@
 }:
 
 {
-  environment.systemPackages = (
-    with pkgs.python313Packages;
-    [
+  environment.systemPackages =
+    (with pkgs.python313Packages; [
       python
-    ]
-  );
+      pip
+      notebook
+    ])
+    ++ (with pkgs; [
+      jupyter
+    ]);
 }
